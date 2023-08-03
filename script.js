037d5b8ba8134113a24275bc8d1c2e0c31071e5f
Rubens Gabriel <rubens-ita@hotmail.com.br>
2023-08-03 13:58:59 -0300

    ajustes de estilo

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ function toggleMode () {
 
   // se tiver sem light mode, manter a imagem normal
 }
+
+/* Load */
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out")
+}
+
+function fadeOut() {
+  setInterval(loader, 3000)
+}
+
+window.onload = fadeOut
